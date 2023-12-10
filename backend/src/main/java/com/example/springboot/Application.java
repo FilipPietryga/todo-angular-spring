b737056001abfaab9f1ca913f32470281c9c25a2
FilipPietryga <filippatrykpietryga@gmail.com>
2023-12-11 00:38:22 +0100

    Introduced lombok and the initialization procedure

--- backend/src/main/java/com/example/springboot/Application.java
+++ backend/src/main/java/com/example/springboot/Application.java
@@ -1,13 +1,30 @@
 package com.example.springboot;
 
+import model.Task;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import repository.TaskRepository;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(TaskRepository taskRepository) {
+		return args -> {
+			Stream.of("do laundry", "finish the homework", "finish housework").forEach(title -> {
+				Task task = new Task(title, title + " right now", new Date());
+			});
+		}
 	}
 
 }
--- backend/src/main/java/model/Task.java
+++ backend/src/main/java/model/Task.java
@@ -3,16 +3,20 @@ package model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    private String title;
-    private String description;
-    private Date deadline;
+    private final String title;
+    private final String description;
+    private final Date deadline;
 }
